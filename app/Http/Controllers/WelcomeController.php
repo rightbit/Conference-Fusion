814defe88ebaff6a2ab73d97c2d79128ca66fe85
Ryan Bouche <rightbit@gmail.com>
2022-08-02 16:58:31 -0600

    Update welcome call message

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Conference;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class WelcomeController extends Controller
 {
@@ -26,11 +27,13 @@ class WelcomeController extends Controller
     {
         //Get the next date for an open call
         $conference = Conference::find(session('selected_conference'));
+        Log::debug("time".time());
+        Log::debug("start_time".strtotime($conference->call_start_date));
         $start_date =  Carbon::parse($conference->call_start_date)->format('F j, Y');
         $welcome_message = 'The date for the next CFP is still being decided.';
         if($conference->call_start_date) {
             $welcome_message = "Submissions for our next conference ({$conference->name}) open on: <u>{$start_date}</u> <br />Register below for a reminder when submissions open.";
-            if (strtotime($conference->call_start_date) >= time() && strtotime($conference->call_end_date) <= time()) {
+            if (strtotime($conference->call_start_date) <= time() && strtotime($conference->call_end_date) >= time()) {
                 $welcome_message = "Submissions for our next conference ({$conference->name}) are now open!<br /> Register (or log in) below to participate.";
             } else if (strtotime($conference->call_end_date) < time()) {
                 $welcome_message = "The submission period for our next conference ({$conference->name}) has closed,<br /> but you may register below to be notified of our next open call.";
