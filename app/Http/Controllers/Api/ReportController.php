052f1e133d23573c5ef902cc75ee7ebea07bc147
rightbit <rightbit@gmail.com>
2023-01-21 12:00:41 -0700

    Update change log report

--- app/Http/Controllers/Api/ReportController.php
+++ app/Http/Controllers/Api/ReportController.php
@@ -53,9 +53,10 @@ class ReportController extends Controller
      */
     public function sessionHistoryList(int $conference_id, Request $request)
     {
-        $history = SessionHistory::where('conference_id', $conference_id)
-            ->when($request->filled('keyword'), function ($query) use ($request) {
-                $query->where('action_short_code', 'LIKE',"%$request->keyword%");
+        $history = SessionHistory::with('user')
+            ->where('conference_id', $conference_id)
+            ->when($request->filled('short_code'), function ($query) use ($request) {
+                $query->where('action_short_code', $request->short_code);
             })
             ->orderBy('created_at', 'DESC')
             ->paginate(25);
--- resources/js/components/admin/reports/SessionHistoryList.vue
+++ resources/js/components/admin/reports/SessionHistoryList.vue
@@ -12,8 +12,12 @@
     <div class="">
         <h2>Change Log</h2>
         <select v-model="codeId" class="form-select w-25 mb-2 me-2 float-end" @change="loadReport">
-            <option value="0">All Codes</option>
-            <option v-for="shortCode in shortCodes" :value="shortCode.name">{{ shortCode.name }}</option>
+            <option value="">All Codes</option>
+            <option value="added_to_schedule">added_to_schedule</option>
+            <option value="updated_schedule">updated_schedule</option>
+            <option value="updated_session_description">updated_session_description</option>
+            <option value="updated_session_participants">updated_session_participants</option>
+            <option value="updated_session_title">updated_session_title</option>
         </select>
         <button class="btn btn-sm btn-link float-end" @click="loadReport"><b><i class="bi bi-arrow-repeat"></i> Reload</b></button>
         <h6>History of important changes to sessions on the schedule, newest to oldest</h6>
@@ -21,7 +25,7 @@
         <div class="log-list border border-1">
             <div v-for="log in logs" class="d-flex p-2 flex-column">
                 <p>
-                    {{ formattedDate(log.created_at) }} | <b>{{ log.action_short_code }}</b>
+                    {{ formattedDate(log.created_at) }} | {{ log.user?.first_name }} {{ log.user?.last_name }} | <b>{{ log.action_short_code }}</b>
                 </p>
                 <p>
                     {{ log.action }}
@@ -42,8 +46,7 @@ export default {
     props: ['conferenceId'],
     data: function() {
         return {
-            shortCodes: {},
-            codeId: 0,
+            codeId: '',
             laravelData: {},
             totalHistory: 0,
             logs: {},
@@ -56,7 +59,6 @@ export default {
         loadReport: function(page = 1) {
             axios.get(`/api/admin/report/session-history-list/${this.conferenceId}`, { params: { short_code: this.codeId, page: page }})
                 .then((response) => {
-                    // console.log(response.data.data);
                     this.logs = response.data.data;
                     this.laravelData = response.data;
                     this.totalHistory = response.data.meta.total;
@@ -64,9 +66,6 @@ export default {
                 .catch((error) => {
                     this.$toast.error(`Could not find the report`);
                 });
-        },
-        changeCodeId: function() {
-
         },
         formattedDate: function (date) {
             return moment(date).format('MMM D, YYYY h:mm:ss a');
